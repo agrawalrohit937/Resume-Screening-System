5eb03921417718b50b02edc164875c3951f4d39c
Rohit Agrawal <agrawalrohit937@gmail.com>
2026-08-08 21:38:54 +0530

    Done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('node_modules/react/') || id.includes('node_modules/react-dom/')) {
+            if (id.includes('node_modules/react/') || id.includes('node_modules/react-dom/') || id.includes('node_modules/scheduler/')) {
               return 'vendor-react-core';
             }
             if (id.includes('react-router') || id.includes('react-router-dom')) {
